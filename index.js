b6f662d378c9233888903305db767325005ef83b
jonnkkz4224 <164797429+jonnkkz4224@users.noreply.github.com>
2024-03-26 10:22:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Watching Nick Community","Listening to Nick Community"];
 
 
 let currentIndex = 0;
